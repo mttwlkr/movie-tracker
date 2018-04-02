fa52fec01e1d846843a13f2bd0553ce2c5fc8a34
chunktooth <om.saetehn@gmail.com>
2018-04-02 17:06:13 -0600

    Greeting user

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -33,6 +33,7 @@ export class App extends Component {
 
     return (
       <div className='nav-menu'>
+      <h3>{`Hey, ${this.props.user.name} !`}</h3>
        <NavLink
           to='/' 
           className='log-out'
