8dffdb778f87d08bd29dfcacacfead505ec607a9
Jared Eklin <jaredeklin@gmail.com>
2018-10-30 18:05:08 -0600

    Update removeFromFavorites with new query url and return info

--- src/cleaners/removeFromFavorites.js
+++ src/cleaners/removeFromFavorites.js
@@ -1,19 +1,17 @@
-export const removeFromFavorites = async (userId, movieId) => {
-  const movieInfo = { 
-    user_id: userId, 
-    movie_id: movieId 
-  };
+const url = process.env.REACT_APP_DATABASE_URL;
 
+export const removeFromFavorites = async (user_id, movie_id) => {
+  const removeInfo = JSON.stringify({ user_id, movie_id });
+  
   try {
-    const response = await fetch(`/api/users/${userId}/favorites/${movieId}/`, {
+    const response = await fetch(`${url}/api/v1/favorites/${removeInfo}`, {
       method: 'DELETE',
-      body: JSON.stringify(movieInfo),
       headers: {
         'content-type': 'application/json'
       }
     });
     const removedData = await response.json();
-
+    
     return removedData;
   } catch (error) {
     throw error;
